00b334393019b35842439972edb5a0cc0c9ae83d
Riley Dallas <rldaggie@gmail.com>
2016-10-03 16:20:38 -0500

    Stub out business logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
+$(document).ready(function () {
+  $('table.board td').on('click', handleClick)
+})
 
+function handleClick(e) {
+  console.log('handleClick()');
+  var cell = $(e.currentTarget)
+  if (cell.text().length > 0) {
+    // Cell is not empty
+    alert('Cell is already taken')
+  } else {
+    // Cell is empty
+    setCellText()
+
+    if (hasWinner()) {
+      // We have a winnner
+      resetBoard()
+      incrementScore()
+      changeCurrentPlayer()
+    } else {
+      // No winner
+      if (catWon()) {
+        alert('Meow')
+        resetBoard()
+      }
+
+      changeCurrentPlayer()
+    }
+  }
+}
+
+function catWon() {
+  console.log('catWon()');
+  return false
+}
+
+function changeCurrentPlayer() {
+  console.log('changeCurrentPlayer()');
+}
+
+function incrementScore() {
+  console.log('incrementScore()');
+}
+
+function resetBoard() {
+  console.log('resetBoard()');
+}
+
+function hasWinner() {
+  console.log('hasWinner()');
+  return false
+}
+
+function setCellText() {
+  console.log('setCellText()');
+}
